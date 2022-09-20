172150920f8dcad5d116c0c40926a3f3ded5d8db
la1av1a <zldqkrktqk@gmail.com>
2022-09-20 16:34:56 +0900

    initial commit

---
+++ src/main/java/com/example/phocazip/PhocazipApplication.java
@@ -0,0 +1,13 @@
+package com.example.phocazip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhocazipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhocazipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/phocazip/PhocazipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phocazip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhocazipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
